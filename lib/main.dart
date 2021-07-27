83c865baf230d13afc10a390c75f77f923da773c
Aviral-tech <nagpalavi2001@gmail.com>
2021-07-27 16:43:09 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,90 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/images.jpg'),
+              ),
+              Text(
+                'Aviral Nagpal',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'ANDROID AND WEB DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child:
+                    ListTile(
+                      leading: Icon(Icons.add_call,
+                        color: Colors.teal,) ,
+                      title: Text('+91 7253070880',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro',
+                          fontWeight: FontWeight.bold,
+                        ),),
+
+                    )
+
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.email,
+                    color: Colors.teal,) ,
+                  title: Text('nagpalavi2001@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                    ),),
+
+                )
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
